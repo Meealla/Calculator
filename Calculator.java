496eb6871b9ee098e14eb9e1d483c6f091397ad6
Meealla <achvokina@mail.ru>
2024-07-02 23:19:51 +0300

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class calc {
+public class Calculator.java {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
