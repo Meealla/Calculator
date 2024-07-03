9d705dd5db8ecbe88699d2c195b9303db185bda3
Meealla <achvokina@mail.ru>
2024-07-03 23:21:42 +0300

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -22,10 +22,11 @@ public class Calculator {
                 num1 = convertToNumber(array[0]);
                 num2 = convertToNumber(array[2]);
 
-
+                if ((num1 < 1 || num1 > 10) || (num2 < 1 || num2 > 10)) {
+                    throw new IllegalArgumentException("Числа должны быть в диапазоне от 1 до 10 включительно");
+                }
             } catch (IllegalArgumentException e) {
-                throw new IllegalArgumentException("диапозон от 1 до 10");
-
+                throw new IllegalArgumentException("Диапазон чисел должен быть от 1 до 10 включительно");
             }
             char operation = array[1].charAt(0);
             int result;
